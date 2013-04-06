71d5ad3ccba4535565b81ad0c2c6c435e194acca
MPiunti <m.piunti@reply.it>
2013-04-07 00:24:24 +0200

    geocode

--- WhlOLD_project/src/main/java/eu/reply/whitehall/client/geocode/GoogleGeoCodeClient.java
+++ WhlOLD_project/src/main/java/eu/reply/whitehall/client/geocode/GoogleGeoCodeClient.java
@@ -1,10 +1,18 @@
 package eu.reply.whitehall.client.geocode;
 
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
+
+import org.xml.sax.InputSource;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
 
 
 public class GoogleGeoCodeClient {
@@ -17,7 +25,28 @@ public class GoogleGeoCodeClient {
 		vars.put("queryStr", key);
 		String result = restTemplate.getForObject("http://maps.googleapis.com/maps/api/geocode/xml?address={queryStr}&sensor=false", 
 				String.class, vars);
+		getLatLong(result);
 		return result;
+	}
+	
+	
+	public void getLatLong(String strstr){
+		try{		
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true); // never forget this!
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new InputSource(new StringReader(strstr)));
+			
+			XPathFactory xFactory = XPathFactory.newInstance();
+			XPath xpath = xFactory.newXPath();
+			XPathExpression expr = xpath.compile("geometry");
+			NodeList nodes = (NodeList)expr.evaluate(doc, XPathConstants.NODESET);
+			for (int i = 0; i < nodes.getLength(); i++) {
+			    System.out.println(nodes.item(i).getNodeValue()); 
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
