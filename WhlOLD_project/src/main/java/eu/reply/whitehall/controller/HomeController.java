98e01fb3a992d4120fe8f4b843099e1cb0ce7abe
MPiunti <m.piunti@reply.it>
2013-09-21 10:38:03 +0200

    CSV View

--- WhlOLD_project/src/main/java/eu/reply/whitehall/controller/HomeController.java
+++ WhlOLD_project/src/main/java/eu/reply/whitehall/controller/HomeController.java
@@ -1,12 +1,19 @@
 package eu.reply.whitehall.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import eu.reply.whitehall.csvIO.CSVView;
 import eu.reply.whitehall.domain.nodes.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 import eu.reply.whitehall.dto.OpenDocumentListDto;
 import eu.reply.whitehall.dto.OpenDocumentMapper;
@@ -49,5 +56,19 @@ public class HomeController {
 		return docService.quit();
 	}
 	
+	
+	@RequestMapping(value="/testCSV")
+	public ModelAndView testCSV() {
+		ModelAndView mav = new ModelAndView();
+		List<String> list_of_values = new ArrayList<String>();
+		list_of_values.add("a");
+		list_of_values.add("b");
+		list_of_values.add("c");
+		mav.addObject("SEPARATOR", ";");
+		mav.addObject("modelKey", list_of_values);
+		View csvView = new CSVView();
+		mav.setView(csvView);
+		return mav;
+	}
 
 }
