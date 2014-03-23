e0b02b067c6014a71f0afe292f33c00070ebd654
m.piunti <m.piunti@reply.it>
2014-03-23 17:05:50 +0100

    test

--- cKan-crawler/ckan-crawler/src/main/java/ckan_crawler/ckan_crawler/Bean1.java
+++ cKan-crawler/ckan-crawler/src/main/java/ckan_crawler/ckan_crawler/Bean1.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Bean1 implements IBean1 {
 
-    private String baseUrl;
+    private String baseUrl, packagelistUrl, grouplistUrl, taglistUrl;
 
-    @Override
+	@Override
     public String hello(String name) {
         return  name + baseUrl + " by mp";
     }
@@ -16,5 +16,39 @@ public class Bean1 implements IBean1 {
     public void setBaseUrl(String url) {
         this.baseUrl = url;
     }
+
+    @Override
+    @Value("${ckan.packagelist.url}")
+    public void setPackagelistUrl(String url) {
+        this.packagelistUrl = url;
+    }    
+
+    @Override
+    @Value("${ckan.grouplist.url}")
+	public void setGrouplistUrl(String url) {
+        this.grouplistUrl = url;
+    }
+
+    @Override
+    @Value("${ckan.taglist.url")
+    public void setTaglistUrl(String url) {
+        this.taglistUrl = url;
+    }
+    
+    public String getBaseUrl() {
+		return baseUrl;
+	}
+
+	public String getPackagelistUrl() {
+		return packagelistUrl;
+	}
+
+	public String getGrouplistUrl() {
+		return grouplistUrl;
+	}
+
+	public String getTaglistUrl() {
+		return taglistUrl;
+	}
     
 }
--- cKan-crawler/ckan-crawler/src/main/java/ckan_crawler/ckan_crawler/IBean1.java
+++ cKan-crawler/ckan-crawler/src/main/java/ckan_crawler/ckan_crawler/IBean1.java
@@ -3,4 +3,7 @@ package ckan_crawler.ckan_crawler;
 public interface IBean1 {
     String hello(String name);
     void setBaseUrl(String var1);
+    public void setPackagelistUrl(String url);
+    public void setGrouplistUrl(String url);
+    public void setTaglistUrl(String url);
 }
